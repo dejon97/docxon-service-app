0b52d816de64a7b82e1ff094f25add641a5dabb4
richCode-Ama <72417184+richCode-Ama@users.noreply.github.com>
2021-11-10 21:53:00 +0000

    documentHelper function

---
+++ helpers/documentHelper.js
@@ -0,0 +1,25 @@
+const { format } = require('util');
+const dotenv = require('dotenv');
+const gc = require('../config/');
+
+dotenv.config();
+const bucket = gc.bucket(process.env.BUCKET_NAME);
+
+const uploaFile = (file) =>
+  new Promise((resolve, reject) => {
+    const { originalname, buffer } = file;
+    const blob = bucket.file(originalname);
+    const blobStream = blob.createWriteStream();
+    blobStream
+      .on('finish', () => {
+        const publicUrl = format(
+          `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+        );
+        resolve(publicUrl);
+      })
+      .on('error', () => {
+        reject(new Error('Unable to upload image, something went wrong'));
+      })
+      .end(buffer);
+  });
+module.exports = uploaFile;
