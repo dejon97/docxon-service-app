c0537aa622ee6085ed0e83555545fc98a5b9ba1d
John McCullough <john.mccullough@navomi.com>
2021-11-23 10:13:54 -0500

    Add logger and rearrange code

---
+++ libs/data/storage.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { Storage } = require('@google-cloud/storage');
+const logger = require('../../utils/logger');
+
+const credentials = JSON.parse(process.env.DB_CONNECTION);
+
+const getStorage = () => {
+  const storage = new Storage({
+    projectId: credentials.project_id,
+    credentials,
+  });
+
+  logger.info('SUCCESS: Storage Connection');
+
+  return storage;
+};
+
+module.exports = {
+  getStorage,
+};
